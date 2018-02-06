6929c93011bf230e4afe0ddbe6f236796f2a7660
Johan Lindqvist <johan.lindqvist@gmail.com>
2018-02-07 00:11:19 +0100

    Added a second wave to the sinus transform filter.

--- generators/sinetransform.cpp
+++ generators/sinetransform.cpp
@@ -14,35 +14,59 @@ SineTransformTextureGenerator::SineTransformTextureGenerator()
 {
    TextureGeneratorSetting angle;
    angle.name = "Angle";
-   angle.defaultvalue = QVariant((double) 0);
+   angle.defaultvalue = QVariant((double) 45);
    angle.min = QVariant((double) 0);
    angle.max = QVariant((double) 90);
    angle.order = 1;
    configurables.insert("angle", angle);
 
    TextureGeneratorSetting frequencyone;
-   frequencyone.name = "Frequency";
-   frequencyone.defaultvalue = QVariant((double) 3);
+   frequencyone.name = "Frequency (1)";
+   frequencyone.defaultvalue = QVariant((double) 0.5);
    frequencyone.min = QVariant((double) 0);
    frequencyone.max = QVariant((double) 10);
    frequencyone.order = 2;
    configurables.insert("frequencyone", frequencyone);
 
    TextureGeneratorSetting amplitudeone;
-   amplitudeone.name = "Amplitude";
-   amplitudeone.defaultvalue = QVariant((double) 10);
-   amplitudeone.min = QVariant((double) -100);
+   amplitudeone.name = "Amplitude (1)";
+   amplitudeone.defaultvalue = QVariant((double) 20);
+   amplitudeone.min = QVariant((double) 0);
    amplitudeone.max = QVariant((double) 100);
    amplitudeone.order = 3;
    configurables.insert("amplitudeone", amplitudeone);
 
    TextureGeneratorSetting offsetone;
-   offsetone.name = "Offset";
-   offsetone.defaultvalue = QVariant((double) -10);
+   offsetone.name = "Offset (1)";
+   offsetone.defaultvalue = QVariant((double) 0);
    offsetone.min = QVariant((double) -360);
    offsetone.max = QVariant((double) 360);
    offsetone.order = 4;
    configurables.insert("offsetone", offsetone);
+
+   TextureGeneratorSetting frequencytwo;
+   frequencytwo.name = "Frequency (2)";
+   frequencytwo.defaultvalue = QVariant((double) 10);
+   frequencytwo.min = QVariant((double) 0);
+   frequencytwo.max = QVariant((double) 10);
+   frequencytwo.order = 5;
+   configurables.insert("frequencytwo", frequencytwo);
+
+   TextureGeneratorSetting amplitudetwo;
+   amplitudetwo.name = "Amplitude (2)";
+   amplitudetwo.defaultvalue = QVariant((double) 6);
+   amplitudetwo.min = QVariant((double) 0);
+   amplitudetwo.max = QVariant((double) 50);
+   amplitudetwo.order = 6;
+   configurables.insert("amplitudetwo", amplitudetwo);
+
+   TextureGeneratorSetting offsettwo;
+   offsettwo.name = "Offset (2)";
+   offsettwo.defaultvalue = QVariant((double) 0);
+   offsettwo.min = QVariant((double) -360);
+   offsettwo.max = QVariant((double) 360);
+   offsettwo.order = 7;
+   configurables.insert("offsettwo", offsettwo);
 }
 
 
@@ -63,6 +87,9 @@ void SineTransformTextureGenerator::generate(QSize size,
    double frequencyone = settings->value("frequencyone").toDouble() * 5 / size.width();
    double amplitudeone = settings->value("amplitudeone").toDouble() * size.width() / 100;
    double offsetone = settings->value("offsetone").toDouble() * 5 / size.width();
+   double frequencytwo = settings->value("frequencytwo").toDouble() * 5 / size.width();
+   double amplitudetwo = settings->value("amplitudetwo").toDouble() * size.width() / 100;
+   double offsettwo = settings->value("offsettwo").toDouble() * 5 / size.width();
 
    TexturePixel* source = sourceimages.value(0)->getData();
    angle = (angle / 180.0) * ((double) M_PI);
@@ -89,7 +116,9 @@ void SineTransformTextureGenerator::generate(QSize size,
          double intersection_y = ((x2 * y1 - x1 * y2) * (y4 - y) - (x4 * y - x * y4) * (y2 - y1)) /
                ((x2 - x1) * (y4 - y) - (x4 - x) * (y2 - y1));
          double distance = sqrt((x - intersection_x) * (x - intersection_x) + (y - intersection_y) * (y - intersection_y));
-         double srcDistance = sin(distance * frequencyone + offsetone) * amplitudeone;
+         double srcDistance =
+               sin(distance * frequencyone + offsetone) * amplitudeone +
+               sin(distance * frequencytwo + offsettwo) * amplitudetwo;
          x4 = x;
          y4 = y - srcDistance;
          int xpos = ((x4 - x) * cos(angle)) - ((y4 - y) * sin(angle)) + x;
